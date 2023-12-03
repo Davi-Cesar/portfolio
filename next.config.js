f9ab7ebb2cfe6c2e9d2f8a1b274ddcb25b496b5b
Davi-Cesar <davicesar2515@gmail.com>
2023-12-02 21:19:15 -0300

    fix: assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
+const prod = process.env.NODE_ENV === "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: prod ? "https://davi-cesar.github.io/portifolio/" : "",
   reactStrictMode: true,
 };
 
-module.exports = {
-  reactStrictMode: true,
-  // outras opções permitidas
-};
+// module.exports = {
+//   reactStrictMode: true,
+//   // outras opções permitidas
+// };
 
 module.exports = nextConfig;
