365e9bd1184ba521470b92790c5b0b42d0c91840
Davi-Cesar <davicesar2515@gmail.com>
2023-12-02 21:03:16 -0300

    fix: path css style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { theme } from "@/styles/theme";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import React from "react";
-import "../../public/styles/themaCode.css";
+import "../styles/themaCode.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
