5cb3623dc901fb7267165071ac15ffd145818ca6
Davi-Cesar <davicesar2515@gmail.com>
2024-02-16 21:47:50 -0300

    fix: add resume and change works

--- src/components/WorkItem.tsx
+++ src/components/WorkItem.tsx
@@ -1,141 +1,123 @@
 import {
-  Flex,
   GridItem,
   Box,
   Image,
   Text,
-  HStack,
   useBreakpointValue,
   useMediaQuery,
-  Stack,
+  Grid,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
 import Link from "next/link";
+import { useState } from "react";
 interface WorkProps {
   image: string;
   name: string;
   text: string;
   number: number;
   link: string;
+  work: string;
 }
 
-export function WorkItem({ link, number, image, name, text }: WorkProps) {
-  const [isLargerThan1280] = useMediaQuery("(min-width: 1280px)");
-
+export function WorkItem({ link, number, image, name, text, work }: WorkProps) {
   return (
     <GridItem
       as={motion.div}
-      rowSpan={2}
-      colSpan={1}
-      h="100%"
-      p="2rem"
+      h="250px"
       w="100%"
       bgColor="rgba(3, 9, 23, 0.055)"
       border="2px solid rgba(95, 95, 95, 0.075)"
-      borderRadius="20px"
-      backdropFilter="auto"
-      backdropBlur="40px"
       justifySelf="center"
-      whileHover={{
+      bgSize="cover"
+      bgPosition="center"
+      bgRepeat="no-repeat"
+      _hover={{
         boxShadow: "-10px 0 20px -10px #429aff",
         borderLeft: "2px solid #429aff",
       }}
     >
-      {isLargerThan1280 ? (
-        <HStack spacing="24px">
-          <Box w={{ base: "100%", sm: "100%", md: "20%" }} h="100%" m="0 auto">
-            <Text fontWeight="thin" fontSize="50px" color="gray.700">
-              {number}
-            </Text>
-          </Box>
-          <Box w="100%" h="100%" m="0 auto" flexDirection="column">
-            <Image
-              src={`works/${image}`}
-              boxSize="220px"
-              alt="cecane"
-              objectFit="contain"
-              overflow="hidden"
-            />
-          </Box>
-          <Flex
-            direction="column"
-            justifyContent="center"
-            w="100%"
-            h="100%"
-            m="0 auto"
-          >
-            <Text textTransform="uppercase">{name}</Text>
-            <Text fontSize="1rem" color="gray.500">
-              {text}
-            </Text>
-            <Link href={link}>
-              <Text
-                marginTop="1rem"
-                fontSize="1rem"
-                color="#429aff"
-                fontWeight="medium"
-                _hover={{ letterSpacing: "5px" }}
-                transition="letter-spacing 1s ease"
-              >
-                View more
-              </Text>
-            </Link>
-          </Flex>
-        </HStack>
-      ) : (
-        <Stack spacing="24px">
-          <Box w={{ base: "100%", sm: "100%", md: "20%" }} h="100%" m="0 auto">
-            <Text fontWeight="thin" fontSize="50px" color="gray.700">
-              {number}
-            </Text>
-          </Box>
+      <Grid
+        display="grid"
+        templateColumns="5% repeat(3, 1fr) 40%"
+        gap="10px"
+        justifyItems="center"
+        alignItems="center"
+        w="100%"
+        h="100%"
+      >
+        <Text fontSize="40px" color="gray.700">
+          {number}
+        </Text>
+        <Text fontSize="25px">{name}</Text>
+        <Text textAlign="center" fontWeight="light" color="gray.400">
+          {text}
+        </Text>
+        <Text textAlign="center" fontWeight="light" color="gray.400">
+          {work}
+        </Text>
+        <Box
+          boxSize="100%"
+          overflow="hidden"
+          position="relative"
+          bg="red.100"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          _hover={{
+            ".ballView": {
+              zIndex: "1",
+              transform: "scale(1.2)",
+              transition: "transform 1s ease",
+            },
+            ".imgWork": {
+              filter: "blur(1.2px)",
+              transition: "filter 1s ease",
+            },
+          }}
+        >
           <Box
-            w="100%"
-            h="100%"
-            m="0 auto"
-            flexDirection="column"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
+            as="a"
+            href={link}
+            boxSize="80px"
+            position="absolute"
+            bg="blue.600"
+            opacity="70%"
+            borderRadius="100%"
+            className="ballView"
+            cursor="pointer"
+            transition="transform 2s ease"
           >
-            <Image
-              src={`works/${image}`}
-              boxSize="220px"
-              alt="cecane"
-              objectFit="contain"
-              overflow="hidden"
-            />
-          </Box>
-          <Flex
-            // bg="blue.300"
-            direction="column"
-            justifyContent="center"
-            w="100%"
-            h="100%"
-            m="0 auto"
-
-            //overflow="hidden"
-          >
-            <Text textTransform="uppercase">{name}</Text>
-            <Text fontSize="1rem" color="gray.500">
-              {text}
+            <Text
+              position="relative"
+              textAlign="center"
+              lineHeight="100%"
+              top="50%"
+              left="50%"
+              transform="translate(-50%, -50%)"
+            >
+              View
             </Text>
-            <Link href={link}>
-              <Text
-                marginTop="1rem"
-                fontSize="1rem"
-                color="#429aff"
-                fontWeight="medium"
-                _hover={{ letterSpacing: "5px" }}
-                transition="letter-spacing 1s ease"
-              >
-                View
-              </Text>
-            </Link>
-          </Flex>
-        </Stack>
-      )}
+          </Box>
+
+          <Image
+            src={`works/${image}`}
+            alt={name}
+            boxSize="100%"
+            objectFit="cover"
+            objectPosition="cover"
+            className="imgWork"
+            filter="blur(0px)"
+            transition="0.5s ease"
+            _hover={{
+              ".imgWork": {
+                filter: "blur(1px)",
+                transition: "filter 0.5s",
+              },
+            }}
+          />
+        </Box>
+      </Grid>
     </GridItem>
   );
 }
--- src/pages/Works/works.tsx
+++ src/pages/Works/works.tsx
@@ -4,82 +4,68 @@ import { Box, Text, Grid, GridItem } from "@chakra-ui/react";
 export default function Works() {
   return (
     <>
-      <Grid
-        h="100%"
-        w="100%"
-        p="2rem 0rem"
-        templateRows="repeat(1fr 3fr)"
-        templateColumns={{ base: "1fr", md: "1fr  2fr", lg: "1fr  2fr" }}
-        gap={4}
-        id="works"
-      >
-        <GridItem
-          rowSpan={8}
-          colSpan={1}
-          justifySelf="center"
-          position="-webkit-sticky"
+      <Grid h="100%" w="100%" gap={4} id="works">
+        <Box
+          position="relative"
+          padding="1rem 0rem"
+          boxSize="100%"
+          display="flex"
+          textAlign="center"
+          id="boxElement"
         >
           <Box
-            position="sticky"
-            padding="4rem 1rem"
-            top="0"
-            left="0"
-            borderRadius="100%"
-            display="flex"
-            textAlign="center"
-            id="boxElement"
+            as="h2"
+            top="10px"
+            fontFamily="Saira"
+            fontSize="50px"
+            fontWeight="bold"
+            textAlign="justify"
+            color="cyan.600"
           >
-            <Box
-              as="h2"
-              top="10px"
-              fontFamily="Saira"
-              fontSize="50px"
-              fontWeight="bold"
-              textAlign="justify"
-              color="cyan.600"
+            Works
+            <Text
+              fontFamily="Outfit"
+              fontWeight="normal"
+              fontSize="16px"
+              color="gray.500"
+              textAlign="left"
             >
-              Works
-              <Text
-                fontFamily="Outfit"
-                fontWeight="normal"
-                fontSize="16px"
-                color="gray.500"
-                textAlign="left"
-              >
-                During my formation at IMD - UFRN participated in some projects.
-                See some of them:
-              </Text>
-            </Box>
+              During my formation at IMD - UFRN participated in some projects.
+              See some of them:
+            </Text>
           </Box>
-        </GridItem>
+        </Box>
+
         <WorkItem
           number={1}
-          image={"alinnydavi.png"}
+          image={"casamento.svg"}
           name={"Alinny e Davi"}
-          text={"My wedding official website."}
+          work="Design e Desenvolvimento"
+          text={"Wedding Site Event"}
           link={"https://alinny-davi.vercel.app/"}
         />
         <WorkItem
           number={2}
-          image={"cecane.png"}
+          image={"cecane.svg"}
           name={"CECANE"}
+          work="Design e Desenvolvimento"
           text={"Implementation of the official CECANE/UFRN website."}
           link={"https://cecane.ccs.ufrn.br/"}
         />
         <WorkItem
           number={3}
-          image={"worldtrip.png"}
-          name={"worldtrip"}
+          image={"worldtrip.svg"}
+          name={"Worldtrip"}
+          work={"Desenvolvimento"}
           text={"Implementation of a travel website."}
           link={"https://github.com/Davi-Cesar/worldtrip"}
         />
         <WorkItem
           number={4}
-          image={"easyscrum.png"}
+          image={"easyscrum.svg"}
           name={"Easy Scrum"}
-          text={
-            "Application for managing project teams, applying Scrum concepts."
-          }
+          work="Design e Desenvolvimento"
+          text={"Application for managing teams, applying Scrum concepts."}
           link={"https://github.com/Kathamala/easy_scrum"}
         />
       </Grid>
